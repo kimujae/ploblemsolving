b15f116982d7f73595f996b03cd6a4731b025845
kimujae <62458513+kimujae@users.noreply.github.com>
2022-11-05 23:51:32 +0900

    [Gold II] Title: 공항, Time: 892 ms, Memory: 109476 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int n, i, count;
+
+        n = scan.nextInt();
+        i = scan.nextInt();
+
+        parent = new int[n + 1];
+        for (int x = 1; x <= n; x++) {
+            parent[x] = x;
+        }
+
+        count = 0;
+        for (int  z= 0; z < i; z++) {
+            int gate = scan.nextInt();
+            int emptyGate = find(gate);
+
+            if (emptyGate == 0) {
+                break;
+            }
+
+            count++;
+            union(emptyGate, emptyGate - 1);
+        }
+
+        System.out.println(count);
+    }
+
+
+
+
+        public static int find(int x) {
+            if (x == parent[x]) {
+                return x;
+            }
+
+            return parent[x] = find(parent[x]);
+        }
+
+        public static void union(int x, int y) {
+            x = find(x);
+            y = find(y);
+
+            if (x != y) {
+                parent[x] = y;
+            }
+        }
+    }
