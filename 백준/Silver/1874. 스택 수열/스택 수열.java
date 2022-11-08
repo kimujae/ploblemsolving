5a93450c593b013f123f801b867bcbb9381e7b49
kimujae <62458513+kimujae@users.noreply.github.com>
2022-11-08 22:13:24 +0900

    [Silver II] Title: 스택 수열, Time: 988 ms, Memory: 106016 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main{
+
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        StringBuilder anssb = new StringBuilder();
+        int n = scan.nextInt();
+        int max, count , input , tmp, next;
+        Stack<Integer> stack = new Stack<>();
+
+        tmp = 0;
+        max = 0;
+        count = 0;
+        next = 0;
+
+        for(int i = 0; i < n ; i++) {
+            input = scan.nextInt();
+            if (max < input) {
+                for (int z = max + 1; z <= input; z++) {
+                    stack.push(z);
+                    anssb.append('+').append('\n');
+                }
+                max = input;
+            }
+            if (stack.peek() == input) {
+                anssb.append('-').append('\n');
+                stack.pop();
+            } else {
+                anssb.setLength(0);
+                anssb.append("NO");
+                break;
+            }
+        }
+        System.out.println(anssb);
+    }
+}
