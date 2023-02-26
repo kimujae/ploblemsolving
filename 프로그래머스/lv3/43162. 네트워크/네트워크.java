53ab8d305f7b3edbb6afb30717794266036f2757
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-02-26 18:18:17 +0900

    [level 3] Title: 네트워크, Time: 0.66 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -0,0 +1,43 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Solution {
+    Queue<Integer> queue = new LinkedList<>();
+    boolean[][] visited; 
+    int[] changeX ={-1, 0, 1, 0};
+    int[] changeY ={0, -1, 0, 1};
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n][n];
+        
+        for(int i = 0; i < n; i++){
+            if(computers[i][i] == 1 && !visited[i][i]){
+                bfs(i, n , computers);
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    void bfs(int node, int n, int[][] computers){  
+        queue.add(node);
+        visited[node][node] = true;
+        
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            
+            for(int i = 0; i < n; i++){
+                if(visited[now][i])
+                    continue;
+                
+                if(computers[now][i] == 0)
+                    continue;
+                
+                queue.add(i);
+                visited[now][i] = true;
+            }   
+        }
+    }
+}
