0f27be4f6dfa10fd7a130f73ad250d55437a9884
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-02-27 20:23:54 +0900

    [Silver I] Title: 정수 삼각형, Time: 240 ms, Memory: 25300 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -5,34 +5,27 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static int[][] triangle;
-    static boolean[][] visited;
+    static int[][] triangle, dp;
 
     public static void main(String[] args) throws IOException{
         n = Integer.parseInt(br.readLine());
         triangle = new int[n][n];
-        visited = new boolean[n][n];
+        dp = new int[n][n];
 
         for(int i = 0; i < n; i ++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j <= i; j++ ){
                triangle[i][j] = Integer.parseInt(st.nextToken());
+               if(i == n-1) dp[i][j] = triangle[i][j];
             }
         }//정수 삼각형 완성
-
         bw.write(String.valueOf(dp(0, 0)));
         bw.flush();
     }
 
     static int dp(int floor, int y){
-        if(floor == n-1){
-            visited[floor][y] = true;
-            return triangle[floor][y];
-        }
-
-        if(visited[floor][y]) return triangle[floor][y];
-        visited[floor][y] = true;
-
-        return triangle[floor][y] += Math.max(dp(floor+1, y), dp(floor+1,y+1));
+        if(floor == n-1) return triangle[floor][y];
+        if(dp[floor][y] != 0) return dp[floor][y];
+        return dp[floor][y]= triangle[floor][y] + Math.max(dp(floor+1, y), dp(floor+1,y+1));
     }
 }
