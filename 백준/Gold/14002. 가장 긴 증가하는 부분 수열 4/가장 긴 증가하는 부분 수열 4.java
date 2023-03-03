52093e64ab70899409e531bae584263294129fbd
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-03-03 16:59:41 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 108 ms, Memory: 12568 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -3,9 +3,9 @@ import java.util.StringTokenizer;
 import java.util.PriorityQueue;
 
 public class Main {
-    static int N, answer, index;
+    static int N, max, index;
     static int[] num_seq;
-    static int[][] dp;
+    static int[] dp;
     static PriorityQueue<Integer> pq = new PriorityQueue<>();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -15,35 +15,35 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         num_seq = new int[N];
         st = new StringTokenizer(br.readLine());
-        dp = new int[N][2];
-        for(int  i = 0; i < N; i ++){
-            dp[i][1]  = - 1;
-        }
-
-
+        dp = new int[N];
+        
         for(int i = 0; i < N; i++){
             num_seq[i] =  Integer.parseInt(st.nextToken());
         }//수열완성
-
+        
+        
         for(int i = 0; i < N; i++){
-            dp[i][0] = 1;
+            dp[i] = 1;
             for(int j = 0; j < i; j++){
                 if(num_seq[i] > num_seq[j])
-                    dp[i][1] = (dp[j][0] + 1 == (dp[i][0] = Math.max(dp[i][0], dp[j][0] +1))) ? j : dp[i][1];
+                    dp[i] = Math.max(dp[i], dp[j] +1);
             }
-            index = (dp[i][0] == (answer = Math.max(answer, dp[i][0])) ? i : index);
+            max = Math.max(max, dp[i]);
         }
-
-
-        pq.add(num_seq[index]);
-        while(dp[index][1] != -1){
-            pq.add(num_seq[dp[index][1]]);
-            index = dp[index][1];
+        
+        index = max;
+        boolean[] visited = new boolean[index];
+        for(int i = N-1; i >= 0; i--){
+            if(!visited[index-1] && dp[i] == index) {
+                pq.add(num_seq[i]);
+                index--;
+            }
+            if(index == 0) break;
         }
-
-        bw.write(String.valueOf(answer));
+        
+        bw.write(String.valueOf(max));
         bw.write('\n');
-        while(!pq.isEmpty()) {
+        while(!pq.isEmpty()){
             bw.write(String.valueOf(pq.poll()));
             bw.write(" ");
         }
