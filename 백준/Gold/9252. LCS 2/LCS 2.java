dc1da4682d8e4db7a74253cf6dc99d82f014bedb
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-03-04 20:47:46 +0900

    [Gold IV] Title: LCS 2, Time: 112 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,55 @@
+import java.io.*;
+public class Main{
+    static String str1, str2;
+    static int[][] dp;
+    static int ans;
+    static char[] ansStr;
+    static boolean[][] visited;
+    static StringBuilder sb;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args)throws IOException{
+        str1 = br.readLine();
+        str2 = br.readLine();
+        dp = new int[str1.length() + 1][str2.length() + 1];
+        visited = new boolean[str1.length() + 1][str2.length() + 1];
+
+
+        ans = dp(str1.length()+1 , str2.length()+1);
+        bw.write(String.valueOf(ans));
+        if(ans != 0) {
+            ansStr = new char[ans];
+            reverseDp(str1.length() , str2.length(), ans-1);
+            bw.write('\n');
+            bw.write(ansStr);
+        }
+        bw.flush();
+    }
+
+/*
+ABCDEF
+BEFDEFACDFABZ
+ */
+    static int dp(int idx1, int idx2){
+        for(int i = 1; i < idx1; i++){
+            for(int j = 1; j < idx2; j++){
+                if(str1.charAt(i - 1) == str2.charAt(j - 1)) dp[i][j] = dp[i-1][j-1] + 1;
+                else dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        return dp[str1.length()][str2.length()];
+    }
+
+    static void reverseDp(int idx1, int idx2, int find) {
+        while (idx1 != 0 && idx2 != 0) {
+            if (dp[idx1][idx2] == dp[idx1-1][idx2]) idx1 = idx1 - 1;
+            else if (dp[idx1 - 1][idx2] == dp[idx1][idx2 - 1]) {
+                ansStr[find] = str1.charAt(idx1-1);
+                idx1 = idx1 - 1;
+                idx2 = idx2 - 1;
+                find--;
+            } else idx2 = idx2 - 1;
+        }
+    }
+}
