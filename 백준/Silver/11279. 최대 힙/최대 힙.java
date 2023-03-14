8c5fad4218200cf9c41d01212aadcc8442567a0b
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-03-14 13:12:02 +0900

    [Silver II] Title: 최대 힙, Time: 304 ms, Memory: 24860 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 
 public class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N;
     static PriorityQueue<Integer> pq  = new PriorityQueue<>(Collections.reverseOrder());
     public static void main(String[] args)throws IOException{
@@ -11,8 +12,15 @@ public class Main{
         for(int i = 0; i < N; i++){
             int num = Integer.parseInt(br.readLine());
             if(num != 0) pq.add(num);
-            else if(num == 0 && pq.isEmpty())System.out.println(0);
-            else System.out.println(pq.poll());
+            else if(num == 0 && pq.isEmpty()){
+                bw.write(String.valueOf(0));
+                bw.write('\n');
+            }
+            else {
+                bw.write(String.valueOf(pq.poll()));
+                bw.write('\n');
+            }
         }
+        bw.flush();
     }
 }
