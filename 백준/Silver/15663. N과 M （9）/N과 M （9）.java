00de0a25f12f77e0049b0848f7ab4b0479bcbfd8
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-03-16 16:02:20 +0900

    [Silver II] Title: N과 M (9), Time: 144 ms, Memory: 25468 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N, M;
+    static int[] nums, comb;
+    static boolean[] visited;
+
+    public static void main(String[] args)throws IOException{
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+        comb = new int[M];
+        visited = new boolean[N];
+
+        st= new StringTokenizer(br.readLine());
+        for(int i =0; i < N; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }//수열 입력 완료
+        Arrays.sort(nums);
+
+        combination(0);
+        System.out.print(sb);
+    }
+
+    static void combination(int depth){
+        if(depth == M){
+            for(int number : comb){
+                sb.append(String.valueOf(number)).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+
+        int prev = 0;
+        for(int i = 0; i < N; i++){
+            //순열이 아니라 수열문제다. 따라서 i = 0부터 시작
+            //같은 depth에서 동일한 수를 선택하지 못하게 해야한다.
+            if(visited[i] || prev == nums[i]) continue;
+            visited[i] = true;
+            prev = comb[depth] = nums[i];
+            combination(depth+1);
+            visited[i] = false;
+        }
+    }
+}
