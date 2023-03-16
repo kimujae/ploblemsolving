a1708323f843b9212fbc0ede1336b3e700033d9a
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-03-17 01:18:47 +0900

    [Gold IV] Title: 최단경로, Time: 728 ms, Memory: 115520 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,5 +1,4 @@
 import java.io.*;
-//import java.util.StringBuilder;
 import java.util.StringTokenizer;
 import java.util.PriorityQueue;
 import java.util.Comparator;
@@ -60,8 +59,16 @@ public class Main {
             int[] curr = pq.poll();
             int node = curr[0];
             int dist = curr[1];
-
+            
             if (dist > cost[node]) continue;
+            //어떤 경우를 걸러내기 위한 조건문인가?
+            /*
+            1의 인접노드 {2, 3}
+            2의 인접노드 {4}
+            3의 인접노드 {4} 
+            일 때, 1-2-4 의 가중치보다 1-3-4의 가중치가 낮을 때(이미 cost[4]는 2회 갱신함) 
+            pq.poll[1] ==  1-2 가중치 + 2-4가중치 이면 1-3가중치 + 3+4 가중치로 갱신된 값보다 크기때문에 continue 패스조건 
+             */
             for (int i = 0; i < graph.get(node).size(); i++) {
                 int next = graph.get(node).get(i)[0];
                 int next_dist = dist + graph.get(node).get(i)[1];
