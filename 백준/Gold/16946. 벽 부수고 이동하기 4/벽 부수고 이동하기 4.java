5f4936a28e02ef952a36d7ddcc7e981afd26b2bf
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-03-26 15:05:07 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 520 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -5,16 +5,14 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
     static Queue<int[]> queue = new LinkedList<>();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
     static int N, M, area, count;
     static boolean[][] visited;
-    static boolean[] visitedArea;
-    static int[][] map;
-    static int[][][] dist;
+    static int[][] map, dist;
     static int[] changeX = {1, 0, -1, 0};
     static int[] changeY = {0, 1, 0, -1};
 
@@ -24,13 +22,16 @@ public class Main{
         M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
         visited = new boolean[N][M];
-        dist = new int[N][M][2];
+        dist = new int[N][M];
 
 
         for(int i = 0; i < N; i++){
             String str= br.readLine();
             for(int j = 0; j < M; j++){
-                if(str.charAt(j) == '1') map[i][j] = 1;
+                if(str.charAt(j) == '1'){
+                   map[i][j] = 1;
+                   dist[i][j] = 1;
+                } 
                 else map[i][j] = 0;
             }
         }// map 정보 입력 완료
@@ -38,31 +39,19 @@ public class Main{
         for(int i = 0; i < N; i++){
             for(int j =0; j < M; j++){
                 if(map[i][j] == 0 && !visited[i][j]) {
-                    count++;
-                    area = 0;
                     visited[i][j] = true;
-                    dfsMovable(i, j);
+                    area++; // 영역개수
+                    count = 0; //영역넓이
+                    getAreaCount(i, j);
                     bfs();
                 }
             }
         }
 
-
-        for(int i = 0; i < N; i++){
-            for(int j =0; j < M; j++) {
-                if (map[i][j] == 1) {
-                    visitedArea = new boolean[count];
-                    dfsWall(i, j);
-                }
-            }
-        }
-
-
-
         for(int i = 0; i < N; i++){
             for(int j =0; j < M; j++){
                 if(map[i][j] == 0) sb.append("0");
-                else sb.append(String.valueOf(dist[i][j][0]));
+                else sb.append(String.valueOf(dist[i][j]));
             }
             sb.append('\n');
         }
@@ -75,37 +64,14 @@ public class Main{
             int[] now = queue.poll();
             int nowX = now[0];
             int nowY = now[1];
-
-            dist[nowX][nowY][0] = area%10;
-            dist[nowX][nowY][1] = count;
+            
+            dist[nowX][nowY] = (dist[nowX][nowY] + count) % 10;
+            visited[nowX][nowY] = false;
         }
     }
 
-    static void dfsWall(int x, int y){
-        dist[x][y][0] = 1;
-        for(int i = 0; i < 4; i++){
-            int nextX = x + changeX[i];
-            int nextY = y + changeY[i];
-
-            if (nextX < 0 || nextX > N - 1 || nextY < 0 || nextY > M - 1)
-                continue;
-
-            if(map[nextX][nextY] != 0 || visitedArea[dist[nextX][nextY][1]-1]) {
-                continue;
-            }
-
-            visitedArea[dist[nextX][nextY][1]-1] = true;
-            dist[x][y][0] = (dist[x][y][0]+dist[nextX][nextY][0]) % 10;
-
-
-        }
-    }
-
-
-    static int dfsMovable(int x, int y){
-        queue.add(new int[]{x, y});
-        area++;
-
+    static int getAreaCount(int x, int y){
+        count++;
 
         for(int i = 0; i < 4; i++) {
             int nextX = x + changeX[i];
@@ -116,18 +82,18 @@ public class Main{
                 continue;
 
 
-            if (visited[nextX][nextY] || map[nextX][nextY] != 0)
+            if (visited[nextX][nextY])
                 continue;
 
+            if(map[nextX][nextY] == 1){
+                queue.add(new int[]{nextX, nextY});
+                visited[nextX][nextY] = true;
+                continue;
+            }
+
             visited[nextX][nextY] = true;
-            dfsMovable(nextX, nextY);
+            getAreaCount(nextX, nextY);
         }
-        return area;
+        return count;
     }
 }
-/*
-3 3
-000
-010
-000
- */
