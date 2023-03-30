7218f59e25d39a4de81a404da3ed46118fdf2866
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-03-30 18:03:29 +0900

    [Gold IV] Title: 가르침, Time: 128 ms, Memory: 11972 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -3,23 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int N,K;
-    static String[] words;
-    static boolean[] visited;
+    static int[] words;
     static int set, ans;
     public static void main(String[] args)throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        visited = new boolean[26];
-        words = new String[N];
+        words = new int[N];
 
         for(int i = 0; i < N; i++){
-            String word = br.readLine();
-            words[i] = word.substring(4, 4 + word.length() - 8);
+            String str = br.readLine();
+            String word = str.substring(4, 4 + str.length() - 8);
+            for(int j = 0; j < word.length(); j++){
+                words[i] |= 1<< (word.charAt(j) -'a');
+            }
         }//단어 입력 완료
 
         if(K >= 5) {
@@ -35,7 +36,6 @@ public class Main {
         for(int i = 0; i < word.length(); i++){
             int num = word.charAt(i) - 'a';
             set |= 1<<num;
-            visited[num] = true;
         }
     }
 
@@ -43,25 +43,19 @@ public class Main {
         if(depth == K-5){
             int read = 0;
             for(int i = 0; i < N; i++){
-                boolean r = true;
-                for(int j = 0 ; j < words[i].length(); j++){
-                    if((set & 1 << (words[i].charAt(j)-'a')) == 0) {
-                        r  = false;
-                        break;
-                    }
-                }
-                if(r) read++;
+                if((set & words[i]) == words[i]) read++;
             }
             ans = Math.max(ans, read);
             return;
         }
 
         for(int i = num; i < 26; i++){
+            // i =0 이 아니라 num이다.....
             if((set & 1<<i) != 0)
                 continue;
 
             set |= 1<<i;
-            dfs(depth+1, i);
+            dfs(depth+1, i+1);
             set &= ~(1<<i);
         }
     }
