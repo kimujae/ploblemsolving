1d678cde63a87482d789f9eb54ebb4c8ebbde153
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-04-13 10:58:15 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.35 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,64 +1,29 @@
-import java.util.*;
 class Solution {
-    ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
-    boolean[] visited;
-    int count;
-    
+    int N, min;
+    int[][] map;
+    int[] vst;
+    int dfs(int n){
+        vst[n] = 1;
+        int child = 1;
+        for(int i = 1; i <= N; i++) {
+            if(vst[i] == 0 && map[n][i] == 1) {
+                vst[i] = 1;
+                child += dfs(i);
+            }
+        }
+        min = Math.min(min, Math.abs(child - (N - child)));
+        return child;
+    }
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        
-        for(int i = 0; i < n+1; i++){
-            tree.add(new ArrayList<>());
+        N = n;
+        min = n;
+        map = new int[n+1][n+1];
+        vst = new int[n+1];
+        for(int[] wire : wires) {
+            int a = wire[0], b = wire[1];
+            map[a][b] = map[b][a] = 1;
         }
-        
-        for(int i = 0; i < wires.length; i++){
-            int node1 = wires[i][0];
-            int node2 = wires[i][1];
-            
-            tree.get(node1).add(node2);
-            tree.get(node2).add(node1);
-        }
-        visited = new boolean[n+1];
-        
-        int a1 =0, a2 =  0;
-        for(int i = 0; i < wires.length;i++){
-            int node1 = wires[i][0];
-            int node2 = wires[i][1];
-            
-            init(node2, n);
-            dfs(node1);
-            a1 = count;
-            
-            init(node1, n);
-            dfs(node2);
-            a2 = count;
-            
-            answer = Math.min(answer, Math.abs(a1-a2));
-        }
-        
-        
-        return answer;
-    }
-    
-    void init(int node, int n){
-        count = 0;
-        visited =new boolean[n+1];
-        visited[node] = true;
-    }
-    
-    
-    
-    void dfs(int now){
-        visited[now] = true;
-        count++;
-        
-        for(int i = 0; i < tree.get(now).size(); i++){
-            int next = tree.get(now).get(i);
-            
-            if(visited[next])
-                continue;
-            
-            dfs(next);
-        }
+        dfs(1);
+        return min;
     }
 }
