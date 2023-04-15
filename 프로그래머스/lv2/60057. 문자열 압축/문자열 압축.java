4e7472ada16fad63539766e3bca96464be052a15
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-04-15 23:51:21 +0900

    [level 2] Title: 문자열 압축, Time: 0.04 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -1,112 +1,53 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+/*
+그 문자의 개수와 반복되는 값으로 표현 -> 더 짧은 문자열로 줄여서 표현
+문자열을 1개 이상의 단위로 잘라서 압축하여 더 짧은 문자열로 표현할 수 있는지 방법을 찾아보려한다.
 
-public class Solution {
 
-    public static Queue<String> queue = new LinkedList<String>();
-    public static String compressure;
+문자열 단위 : 1~s.length()/2 까지 반복 
+문자열 순회탐색 -> 압축 반복
 
-    public static int solution(String s) {
+*/
+class Solution {
+    public int solution(String s) {
         int answer = 0;
-        /*
-        1. 문자열자르기를 1부터 문자열의길이/2까지 반복수행
-        2. 각 반복 수행동안 할 것은
-        2-1. 자른 문자열(compare)을 기억하고 있다가 바로 다음(part)에 자른 문자열이 나오는지 확인
-        2-2. 나온다면, count++
-        2-3. 나오지 않는다면, count == 1 이면 이전 자른 str += 문자열 count > 1이면 count+문자열
-             새로 자른 문자열을 변수에 저장하여 수행
-        min > str.length 이면 min에 최소값 저장
-        3. 예외: 자른 부분 문자열의 길이보다 남은 문자열의 길이가 짧은 경우.
-            예외 : 비교하려는 문자열의 길이가 비교 문자열보다 길이가 짧은 경우.
-         */
-
-
-        String part;
-        String compare;
-        int count = 0;
-        int curIndex = 0;
-        answer = Integer.MAX_VALUE;
-        if(s.length()==1) return 1;
-
-        for(int cutLen = 1; cutLen <= s.length()/2 ;cutLen++) {
-            part ="";
-            compare ="";
-            compressure = "";
-            count = 0;
-            // 문자열 자르기 반복 수행
-            for(int i = 0; i < s.length(); i++){
-                if(s.charAt(i) == '"');
-                else queue.add(s.charAt(i)+"");
-            }
-
-            for (int iter = 0; iter < cutLen; iter++) {
-                compare += queue.poll();
-            }
-
-            for (int iter = 0; iter < cutLen; iter++) {
-                part += queue.poll();
-            }
-
-            while(true) {
-                count = 0;
-                while (compare.equals(part)) {
-                    // 같은것이 나오지 않을 때 까지 반복 비교
-                    count++;
-                    if(queue.isEmpty()) break;
-                    if (queue.size() < cutLen) break;
-                    part = "";
-                    for (int iter = 0; iter < cutLen; iter++) {
-                        //문자열 자르기 수행
-                        part += queue.poll();
-                    }
-                }
-                if(count == 0){
-                    // 반복되는 것이 하나도 없을때
-                    compressure += compare;
-                    compare = part;
-                    part ="";
-                    if(queue.isEmpty()){
-                        compressure += compare;
-                        break;
-                    }
-                    else if (queue.size() < cutLen){
-                        compressure += compare;
-                        while (!queue.isEmpty()){
-                            compressure += queue.poll();
-                        }
-                        break;
-                    }
-                    else {
-                        for (int iter = 0; iter < cutLen; iter++) {
-                            part += queue.poll();
-                        }
-                    }
-                }
-                else{
-                    // 반복되는 것이 하나라도 있을 때
-                    compressure += (count+1) + compare;
-                    //
-                    if(queue.isEmpty()) break;
-                    else if (queue.size() < cutLen) {
-                        if(!compare.equals(part)) compressure += part;
-                        while (!queue.isEmpty()){
-                            compressure += queue.poll();
-                        }
-                        break;
-                    }
-                    else {
-                        compare = part;
-                        part="";
-                        for (int iter = 0; iter < cutLen; iter++) {
-                            part += queue.poll();
-                        }
-                    }
-                }
-            }
-            //System.out.println(compressure);
-            if(compressure.length() < answer) answer = compressure.length();
-        }
+        if(s.length() == 1) answer = 1;
+        else answer = solve(s);
         return answer;
+    }
+    
+    int solve(String s){
+        int min = Integer.MAX_VALUE;
+        
+        for(int i = 1; i <= s.length()/2; i++){
+            // 문자열 trim 단위 
+            int iter = 1;// 반복 문자열 횟수
+            int len = 0;
+            String curr  = "";
+            String prev = s.substring(0, i);
+            int idx = i;
+            
+            while(true){
+                // 문자열 탐색
+                if(idx+i > s.length()) {
+                    // 반복 문자열 횟수 길이 + trim문자열 길이  + 잔여 문자열 
+                    if(iter > 1)len += String.valueOf(iter).length() + i + s.length()-idx;
+                    else len += i + s.length()-idx;
+                    break;
+                }
+                
+                curr = s.substring(idx, idx+i); // 현재 문자열 할당
+                if(curr.equals(prev)) iter++; //비교문자열이 맞다면 반복횟수++
+                else{// 비교문자열이 다르다면,
+                    if(iter > 1) len += String.valueOf(iter).length() + i;
+                    else len += i;
+                    
+                    iter = 1;
+                    prev = curr; // 비교문자열 교체
+                }
+                idx = idx+i;
+            }
+            min = Math.min(min, len);
+        }
+        return min;
     }
 }
