eb6ca0e7e9d4899329cd32de23df8993f0225ced
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-09-14 16:43:42 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,38 +1,25 @@
 import java.util.*;
 class Solution {
-    private List<int[]> list = new ArrayList<>();
     private boolean[] visit;
     private int max;
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
         visit = new boolean[dungeons.length];
-        
-        permutation(k, dungeons, list, dungeons.length);
+        permutation(k, dungeons, 0);
         return max;
     }
-    
-    public void permutation(int k , int[][] dungeons, List<int[]> list, int target) {
-        if(list.size() == target) {
-            int adventures = 0;
-            // 던전 탐험 판정로직 
-            for(int[] fatigues : list) {
-                if(k < fatigues[0])break;
-                  
-                k -= fatigues[1];
-                adventures += 1;
-            }
-            max = Math.max(max, adventures);
-            return;
-        }
-        
+
+    public void permutation(int k , int[][] dungeons, int depth) {
         for(int i = 0; i < dungeons.length; i++) {
-            if(visit[i]) continue;
-            
+            if(visit[i] || k < dungeons[i][0]) continue;
+
             visit[i] = true;
-            list.add(dungeons[i]);
-            permutation(k, dungeons, list, target);
-            list.remove(list.size() - 1);
+            k -= dungeons[i][1];
+            permutation(k, dungeons, depth + 1);
+            k += dungeons[i][1];
             visit[i] = false;
         }
+        
+        
+        max = Math.max(max, depth);
     }
 }
