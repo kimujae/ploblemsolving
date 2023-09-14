ad00be43f5ff25131c0a14284d2907872494cf2d
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-09-14 14:21:47 +0900

    [level 1] Title: 최소직사각형, Time: 2.57 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -1,60 +1,22 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        PriorityQueue<int[]> wpq = new PriorityQueue(new Comparator<int[]>(){
-            @Override
-            public int compare(int[] o1, int[] o2){
-                if(o1[0] == o2[0]) return o2[1]- o1[1];
-                return o2[0] - o1[0];
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        int maxidx = -1;
+        for(int i = 0 ; i < sizes.length; i ++) {
+            if(max < sizes[i][0] || max < sizes[i][1]) {
+                maxidx = i;
+                max = Math.max(sizes[i][1], sizes[i][0]);
+
             }
-        });
-        
-        PriorityQueue<int[]> hpq = new PriorityQueue<>(new Comparator<int[]>(){
-            @Override
-            public int compare(int[] o1, int[] o2){
-                if(o1[1] == o2[1]) return o2[0]- o1[0];
-                return o2[1] - o1[1];
-            }
-        });
-        
-        
-        
-        for(int i = 0; i < sizes.length; i++){
-            int x = sizes[i][0];
-            int y = sizes[i][1];
-            
-            wpq.add(new int[]{x, y});
-            hpq.add(new int[]{x, y});
         }
         
-        int wmax = wpq.peek()[0];
-        int hmax = hpq.peek()[1];
-        answer = wmax * hmax;
-        
-        if(wmax > hmax){
-            while(hpq.peek()[0] < hpq.peek()[1]){
-                int[] now = hpq.poll();
-                int x = now[1];
-                int y = now[0];
-                
-                hpq.add(new int[]{x, y}); 
-            }
-            
-            answer = wmax * hpq.peek()[1];
-        }else{
-            while(wpq.peek()[0] > wpq.peek()[1]){
-                int[] now = wpq.poll();
-                int x = now[1];
-                int y = now[0];
-                
-                
-                wpq.add(new int[]{x, y}); 
-            }
-            answer = hmax*wpq.peek()[0];
+        int max2 = 0;
+        for(int i = 0; i < sizes.length; i++) {            
+            max2 = Math.max(max2, Math.min(sizes[i][0], sizes[i][1]));
         }
         
-        
-        return answer;
+        return max * max2;
     }
 }
