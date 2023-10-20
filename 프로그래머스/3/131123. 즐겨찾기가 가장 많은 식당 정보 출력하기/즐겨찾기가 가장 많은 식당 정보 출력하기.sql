dc5d9a35f5604248caf3e19eb7d28f1e79dc63f4
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-10-20 21:48:13 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,12 @@
+
+select food_type, rest_id, rest_name, favorites
+from rest_info
+where (food_type, favorites) IN 
+    (
+    select food_type, max(favorites)
+    from rest_info
+    group by food_type
+    )
+order by food_type desc;
+
+
