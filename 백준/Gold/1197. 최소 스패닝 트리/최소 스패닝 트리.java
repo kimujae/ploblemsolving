034a128a04e67b9b677a713286eb99712234b8a8
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2023-11-12 01:41:47 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 492 ms, Memory: 47504 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,74 +1,78 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main{
-    static boolean[] visited;
-    static PriorityQueue<int[]> pq  = new PriorityQueue<int[]>(new Comparator<int[]>() {
-        @Override
-        public int compare(int[] o1, int[] o2) {
-            if(o1[2] == o2[2])return o1[0] - o2[0];
-            else return o1[2] - o2[2];
-        }
-    });
-    static int V, E, ans;
-    static int[] parent;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-
-    /*
-    pq에 가중치 우선순위 저장
-    union-find로 탐색
-     */
-
-    public static void main(String[] args)throws IOException {
+public class Main {
+    private static BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static int V, E;
+    private static int[] p;
+    private static PriorityQueue<int[]> pq =
+            new PriorityQueue<>(new Comparator<int[]>(){
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return o1[2] - o2[2];
+                }
+            });
+    public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
-        visited = new boolean[V+1];
-        parent = new int[V+1];
 
-        for(int i = 0; i < V+1; i++){
-            parent[i] = i;
-        }
-
-        for(int i = 0; i < E; i++){
+        for(int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            pq.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
-        }//우선순위큐 완성
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
 
-        while(!pq.isEmpty()){
-            int[] graph = pq.poll();
+            pq.add(new int[]{A, B, C});
+        }// pq 초기화
 
-            if(find(graph[0]) == find(graph[1]))
-                continue;
+        initSet();
+        int conn = 0;
+        long dist = 0;
+        while(!pq.isEmpty()) {
+            //if(conn == E - 1) break;
 
-            union(graph[0], graph[1]);
-            ans+= graph[2];
+            int[] now = pq.poll();
+            int s = now[0];
+            int n = now[1];
+            int d = now[2];
+
+            if(find(s) == find(n)) continue;
+            union(s, n);
+            dist += d;
+            conn++;
         }
 
-        bw.write(String.valueOf(ans));
-        bw.flush();
+        System.out.print(dist);
     }
 
 
-    static int find(int x){
-        if(x == parent[x]) return x; // 루트노드
-        else{
-            int y = find(parent[x]);
-            parent[x] = y; // 집합의 루트 노드를 반환
-            return y;
+
+    public static void initSet() {
+        p = new int[V+1];
+        for(int i = 1; i < V+1; i++) {
+            p[i] = i;
         }
     }
 
-    static void union(int x, int y){
+    public static int find(int x) {
+        if(p[x] == x) return p[x];
+
+        int y = find(p[x]);
+        p[x] = y;
+        return y;
+    }
+
+    public static void union(int x, int y) {
         x = find(x);
         y = find(y);
 
-        if(x!=y){
-            parent[y] = x;
-        }
+        if(x != y) p[y] = x;
     }
 }
