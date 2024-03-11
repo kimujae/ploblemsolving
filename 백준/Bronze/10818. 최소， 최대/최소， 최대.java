31486d7618a93a8e9f3d9ff69f0bf8681801f0a0
김우재(U Jae  Kim) <62458513+kimujae@users.noreply.github.com>
2024-03-11 21:47:55 +0900

    [Bronze III] Title: 최소, 최대, Time: 520 ms, Memory: 134524 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    static BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < N ;i++) {
+            int num = Integer.parseInt(input[i]);
+            
+            if(num > max) max = num;
+            if(num < min) min = num;
+        }
+        
+        
+        System.out.println(min + " " + max);
+    }
+}
